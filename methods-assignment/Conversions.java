c3b1b4bb6666a38690807606884f7adec4b38773
Dhairya Gupta <dhairyag23@gmail.com>
2022-10-06 20:04:41 -0500

    comments.

--- methods-assignment/Conversions.java
+++ methods-assignment/Conversions.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 
 public class Conversions {
     public static void main(String[] args) {
+
+        // setup the scanner
         Scanner input = new Scanner(System.in);
+
+        // set variables we will input
         int conversionChoice, number;
 
+        // ask user for their choice
         System.out.println(
                 "What would you like to convert? (1) feet to inches (2) centimeters to inches (3) miles to meters (4) miles to feet (5) pounds to ounces (6) kilowatts to horsepower");
         conversionChoice = input.nextInt();
 
+        // ask user for number they want to convert
         System.out.println("Enter the number you would like to convert");
         number = input.nextInt();
 
@@ -37,6 +43,9 @@ public class Conversions {
         }
 
     }
+
+    // create methods for each conversion and retunrn the valu, the method itself
+    // will not print anything: we will print the return value in the main method
 
     public static int feetToInches(int number) {
         return number * 12;
--- methods-assignment/EuclideanAlgorithm.java
+++ methods-assignment/EuclideanAlgorithm.java
@@ -2,14 +2,20 @@ import java.util.Scanner;
 
 public class EuclideanAlgorithm {
     public static void main(String[] args) {
+
+        // setup variables for the input of the user's numbers
         int numOne, numTwo, remainder = -1;
         Scanner input = new Scanner(System.in);
+
+        // ask user for the numbers
         System.out.println("Enter the first number: ");
         numOne = input.nextInt();
         System.out.println("Enter the second number: ");
         numTwo = input.nextInt();
 
+        // while the remainder isn't 0, run this loop
         while (remainder != 0) {
+            // run the method to get the remainder of the two numbers
             remainder = remainder(numOne, numTwo);
             if (remainder != 0) {
                 numOne = numTwo;
@@ -17,9 +23,11 @@ public class EuclideanAlgorithm {
             }
         }
 
+        // print the GCD
         System.out.println("The greatest common denominator is " + numTwo);
     }
 
+    // return the remainder between two numbers
     public static int remainder(int numOne, int numTwo) {
         return numOne % numTwo;
     }
