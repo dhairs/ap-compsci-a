bac6960bb90533a22b7ee5a06274b2516b7d11ad
Dhairya Gupta <dhairyag23@gmail.com>
2022-10-13 11:05:23 -0500

    ok i commented

--- recursion-assignment/RecursionOne.java
+++ recursion-assignment/RecursionOne.java
@@ -1,18 +1,33 @@
+// Dhairya Gupta   
+
 import java.util.Scanner;
 
 public class RecursionOne {
     public static void main(String[] args) {
+        // Set up scanner for reading in
         Scanner input = new Scanner(System.in);
+        // Read in as a string because it's easier to reverse a string than an integer
         String number;
+
+        // Read in number as string
         System.out.print("Enter a number: ");
         number = input.nextLine();
 
+        // Print the result of the reverse() method (which runs recursively)
         System.out.println(reverse(number));
     }
 
+    // Method to reverse a string, takes String number as argument
     public static String reverse(String number) {
+
+        // If the string is just 1 character long, return it (this will the base case,
+        // but also preps us for a possible edge case)
         if (number.length() == 1)
             return number;
+
+        // if not just one char long, we find the last character and add it to the rest
+        // of the string using the reverse() method again, it eventually reaches a 1
+        // length string and that ends up being the back of the returned string
         return number.charAt(number.length() - 1) + reverse(number.substring(0, number.length() - 1));
     }
 }
--- recursion-assignment/RecursionTwo.java
+++ recursion-assignment/RecursionTwo.java
@@ -1,19 +1,35 @@
+// Dhairya Gupta
+
 import java.util.Scanner;
 
 public class RecursionTwo {
     public static void main(String[] args) {
+
+        // Accept user input
         Scanner input = new Scanner(System.in);
+
+        // Accepts integer values
         int num, pow;
+
+        // Read in the numbers
         System.out.print("Enter a number: ");
         num = input.nextInt();
         System.out.print("Enter a power: ");
         pow = input.nextInt();
+
+        // Print the result of the power() method (which runs recursively)
         System.out.println(pow(num, pow));
     }
 
+    // Define pow() method which takes an integer and the pow(er) it should be
+    // raised to as arguments, integers
     public static int pow(int num, int pow) {
+        // If the power is equal to 1, just return the number, this is our base case
         if (pow == 1)
             return num;
+        // otherwise, return the number multiplied by the result of the pow() method
+        // with 1 less power, this runs recursviely until the power equals 1 (note: it
+        // does not work with negative numbers)
         return num * pow(num, pow - 1);
     }
 }
